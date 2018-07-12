99a5b7e69ffe30260709e3fa28bda543b4fed467
Peter_hujy <hujy@shanghaitech.edu.cn>
2018-07-12 14:52:16 +0800

    Add collider for gates, Scoreboard

---
+++ TA Simulator/Assets/ScoreBoard.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreBoard : MonoBehaviour {
+    private Text txt;
+    [HideInInspector]
+    public int Player1_score;
+    [HideInInspector]
+    public int Player2_score;
+    
+    // Use this for initialization
+    void Start () {
+        txt = GetComponent<Text>();
+        Player1_score = 0;
+        Player2_score = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        txt.text = Player1_score.ToString() + " : "+ Player2_score.ToString();
+	}
+}
--- TA Simulator/Assets/Scripts/Game1Ball.cs
+++ TA Simulator/Assets/Scripts/Game1Ball.cs
@@ -2,10 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
+
 public class Game1Ball : MonoBehaviour
 {
     private Rigidbody2D rig2D;
-
+    public ScoreBoard scoreboard;
     private void Awake()
     {
         rig2D = GetComponent<Rigidbody2D>();
@@ -14,5 +16,12 @@ public class Game1Ball : MonoBehaviour
     public void Update()
     {
         rig2D.velocity = new Vector2(Mathf.Min(7, rig2D.velocity.x), Mathf.Min(7, rig2D.velocity.y));
+    }
+
+    void OnTriggerEnter2D(Collider2D col) {
+        if (col.gameObject.tag == "gate1") { scoreboard.Player1_score += 1; }
+        if (col.gameObject.tag == "gate2") { scoreboard.Player2_score += 1; }
+
+
     }
 }
